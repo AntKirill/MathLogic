1433410fc3be0b886dbc758ccd72d3d280a647f3
kant97 <kirant9797@gmail.com>
2017-04-11 11:23:40 +0300

    beautiful main

--- Task2/hw2_main.cpp
+++ Task2/hw2_main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "proofing_library/parser.h"
-#include "proofing_library/checker.h"
-#include "proofing_library/axioms_and_assumptions.h"
-#include "proofing_library/proofer.h"
+#include <parser.h>
+#include <checker.h>
+#include <axioms_and_assumptions.h>
+#include <proofer.h>
 #include <fstream>
 
 using namespace std;
@@ -12,24 +12,23 @@ int main() {
     freopen("out.txt", "w", stdout);
     setlocale(LC_ALL, "Russian");
     time(0);
+
     string s;
     getline(fin, s);
 
-    string all_fun_is_for;
     parser p;
-
-    //cerr << p.parse(s)->expression << endl;
     axioms_and_assumptions axs(s);
     proofer generator(axs.last_assumption);
     checker ch(axs);
 
     bool ok = true;
 
-    cout << s << endl;
+    //solution is here
+    proofer::generate_caption(axs.parsed_assumptions, axs.all_fun_is_for);
     while (getline(fin, s)) {
         std::shared_ptr<node> root = p.parse(s);
         if (!ch.check(root)) {
-            cerr << "ERROR!";
+            cerr << "ERROR! ";
             freopen("out.txt", "w", stdout);
             printf("Вывод некорректен начиная с формулы номер %d\n", ch.get_line_number());
             ok = false;
